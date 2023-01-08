59bc640019e521dc683c05fcd1b9f8bad26f4759
Enrique Betancourt <fis.ebetancourt@gmail.com>
2023-01-08 13:07:27 -0600

    added create test

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,7 +1,52 @@
 import unittest
+import requests
+
+from api.v1 import config as config
 from api.v1.database import DatabaseClient as db
+from utils import create_test_user, login, delete_user, random_text
 
 
 class UnitTetsCases(unittest.TestCase):
+    _test_user = None
+    _token = None
+    base_url = f"{config.SERVICE_BASE_URL}:{config.SERVICE_PORT}"
+
     def setup(self):
-        pass
+        # Create the test user and register it into the Users table
+        self._test_user = create_test_user()
+
+        # Log in with the test user and fetch the auth token
+        response = login(self._test_user['name'], self._test_user['password'])
+        self._token = response["access_token"]
+
+    def shutdown(self):
+        # Before shutting down, delete the test user and remove the record
+        delete_user(self.test_user["_id"])
+
+    def test_create_event(self):
+        url = f"{self.base_url}/events"
+        headers = {
+            "Content-type": "text/json",
+            "Authorization": self._token
+        }
+        body = {
+            "name": f"TestName-{random_text(6)}",
+            "details": f"TestDetails-{random_text(12)}",
+            "event_type": "TestEventType",
+        }
+        response = requests.post(url=url, headers=headers, body=body)
+
+        # Assert the api response is successful
+        assert response.status == 200
+
+        # Validate that the event was created
+        created_event = db.database["events"].find_one({"name": body["name"]})
+
+        assert created_event.name == body["name"]
+        assert created_event.details == body["details"]
+        assert created_event.event_type == body["event_type"]
+        assert bool(created_event._id)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/utils.py
+++ tests/utils.py
@@ -1,19 +1,25 @@
 import requests
+import string
+
 from api.v1 import config as config
 from api.v1.database import DatabaseClient as db
+from random import choice
 
-
+TEXT_POOL = string.ascii_letters + string.digits + string.punctuation
 
 def create_test_user():
-    username = "test-user"
-    password = "12345678910"
+    username = f"test-{random_text(string.ascii_letters)}"
+    password = ''.join(random_text(12))
     result_id = db.database["users"].insert_one({
         "usename": username,
         "password": password,
         "disable": False
     })
     created_user = db.database["users"].find_one({"_id": result_id})
-    return bool(created_user)
+    if not created_user:
+        raise Exception("The test user wasn't created, cannot proceed with the test")
+    created_user["password"] = password
+    return created_user
 
 def login(username: str, password: str):
     headers = {
@@ -29,5 +35,18 @@ def login(username: str, password: str):
                            headers=headers,
                            body=body)
 
-    return result
+    return result.json()
     
+
+def delete_user(id: str) -> bool:
+    """Deletes the given user
+
+    :param id: The id of the user to delete
+    :returns: True if the user was deleted, otherwise, False
+    """
+    return db.database["users"].delete_one({"_id": id})
+    
+
+def random_text(text_map: str, length: int = 6) -> str:
+    text_map = TEXT_POOL if not text_map else text_map
+    return "".join(choice(string.ascii_letters + string.digits) for _ in range(length))
